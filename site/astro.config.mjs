c6bc4ba6bc9822f90a189a9261863790076dd351
pikesley <sam.pikesley@gmail.com>
2023-03-19 22:42:43 +0000

    Beautify output

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,4 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+import htmlBeautifier from "astro-html-beautifier";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://carbwire.pikesley.org",
+  integrations: [
+    htmlBeautifier({
+      indent_size: 2,
+      wrap_line_length: 88,
+      jslint_happy: true,
+      preserve_newlines: false,
+    }),
+  ],
+});
